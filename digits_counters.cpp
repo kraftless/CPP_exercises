776262e111bbd39af55e916c7b69da7c9df772eb
RobinLiang420 <liangyb0420@163.com>
2017-10-02 21:48:00 -0500

    optimized digits initial value
    optimized digits initial value

--- digits_counters.cpp
+++ digits_counters.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
 	int num;
 	cin >> num;
-	int digits = 0;
+	int digits = 1;
 	while (num / 10) 
 	{   
 		digits++;
 		num = num / 10; // num/10 will be traunted to integer and stored for next loop
 	}
 
-	cout << "The digits of input is:"<< digits+1 << endl;   
+	cout << "The digits of input is:"<< digits << endl;   
 	
 	return 0;
 }
